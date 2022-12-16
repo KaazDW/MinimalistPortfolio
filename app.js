dabbb6ba8215556cfdb3ebf72ff193495dacd8b0
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-12-16 22:05:32 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById('project').style.display = "none";
+
+function index(){
+    document.getElementById('index').style.display = "block";
+    document.getElementById('project').style.display = "none";
+
+}
+function project(){
+    document.getElementById('index').style.display = "none";
+    document.getElementById('project').style.display = "block";
+}
