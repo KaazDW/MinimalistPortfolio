8d8181b205666b59ac877d677d4e9f094250b240
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-12-19 05:23:12 +0100

    final update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
+//functions for changing display between the 2 pages
 function index(){
     document.getElementById('index').style.display = "block"
     document.getElementById('project').style.display = "none"
-
 }
 function project(){
     document.getElementById('index').style.display = "none"
@@ -15,7 +15,6 @@ let tday = today.getDate()
 let tmonth = today.getMonth() + 1
 let tyear = today.getFullYear()
 console.log('Today Date : ', tday, tmonth, tyear)
-
 
 const date = new Date()
 
@@ -36,8 +35,7 @@ console.log('> Age :', year, dayvalue)
 document.getElementById('age').innerHTML = year + ' years'
 document.getElementById('day').innerHTML = dayvalue + ' days'
 
-
-
+// CSS settings for the first open of the page
 document.getElementById('project').style.display = "none"
 document.getElementById('card-content1').style.display = "none"
 document.getElementById('card-content2').style.display = 'block'
@@ -47,21 +45,23 @@ document.getElementById('minimized1').style.display = 'flex'
 document.getElementById('minimized2').style.display = 'none'
 document.getElementById('minimized3').style.display = 'flex'
 
+//card display
 function projectdisplay(x){
     //x = the card clicked on
     if(x === 1){
+        //display '[ open ]' tag of each project's card
         document.getElementById('minimized1').style.display = 'none'
         document.getElementById('minimized2').style.display = 'flex'
         document.getElementById('minimized3').style.display = 'flex'
-        //size redefinition
+        //card's size redefinition
         document.getElementById('card1').style.width = '70%'
         document.getElementById('card2').style.width = '33%'
         document.getElementById('card3').style.width = '33%'
-        //content display none of the other card
+        //content display:none of the other card
         document.getElementById('card-content2').style.display = 'none'
         document.getElementById('card-content3').style.display = 'none'
-        //display the content of the actual choosen card
-        setTimeout(() => {  
+        //display the content of the choosen card
+        setTimeout(() => {  //I delay the display to make the animation more fluid
             document.getElementById('card-content1').style.display = 'block'
         }, 200);
     }
